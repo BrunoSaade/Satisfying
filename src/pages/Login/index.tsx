71179fa630eea75661499f8c10f99321bd7a45e8
BrunoSaade <brsaade@gmail.com>
2023-09-04 21:05:28 -0300

    Style: Added icon

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Container } from "../../styles/styles";
 import { Text } from "react-native";
+import Icon from 'react-native-vector-icons/MaterialIcons'
 
 export default function Login() {
   return (
     <Container>
+      <Icon name="mood" size={60} color="red" />
       <Text>Ola</Text>
     </Container>
   );
