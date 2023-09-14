e7bd33cece7c3f73446d98d3fa13d4819f8c9687
BrunoSaade <brsaade@gmail.com>
2023-09-14 14:57:00 -0300

    Chore: Converted file js to tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Login from "./pages/Login";
-import { Container } from "./styles/styles";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import Home from "./pages/Home";
@@ -10,13 +9,10 @@ const Stack = createStackNavigator()
 
 export default function App() {
   return (
-    // <Container>
-    //   <Login></Login>
-    // </Container>
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Login" component={Login} options={{headerShown: false}}/>
-        <Stack.Screen name="Drawer" component={Drawer} />
+        <Stack.Screen name="Drawer" component={Drawer} options={{headerShown: false}}/>
         <Stack.Screen name="Home" component={Home} />
       </Stack.Navigator>
     </NavigationContainer>
