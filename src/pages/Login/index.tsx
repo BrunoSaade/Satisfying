08a50dcabd8d093aab0db17d30758a18791306f0
BrunoSaade <brsaade@gmail.com>
2023-09-15 10:24:58 -0300

    Refactor: Applied a generic function to redirect to other pages, fixed styles

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -13,9 +13,9 @@ import RContainer from '../../components/RContainer';
 
 export default function Login(props: any) {
 
-  const handleLogin = () => {
-    props.navigation.push('Drawer')
-  };
+  function handleToPage(page: string) {
+    props.navigation.push(page)
+  }
 
   return (
     <RContainer>
@@ -25,14 +25,14 @@ export default function Login(props: any) {
             <S.TextDefault style={styles.view_logo__text}>Satisfying.you</S.TextDefault>
             <Icon name="sentiment-satisfied" size={60} color="white" />
           </S.Container>
-          <S.Container>
+          <S.Container style={{gap: 15}}>
             <RInput label="E-mail" placeholder="E-mail"/>
             <RInput label="Senha" placeholder="Digite sua senha" isPassword/>
-            <RButton style={{marginTop: 30}} label="Entrar" color="success" onPress={handleLogin}/>
+            <RButton style={{marginTop: 30}} label="Entrar" color="success" onPress={() => handleToPage('Drawer')}/>
           </S.Container>
-          <S.Container style={{marginTop: 40}}>
-            <RButton customHeight="37" label="Criar minha conta" color="neutral" onPress={handleLogin}/>
-            <RButton style={{marginTop: 15}} customHeight="37" label="Esqueci minha senha" color="info" onPress={handleLogin}/>
+          <S.Container style={{marginTop: 70}}>
+            <RButton customHeight={37} label="Criar minha conta" color="info" onPress={() => handleToPage('Nova Conta')}/>
+            <RButton style={{marginTop: 15}} customHeight={37} label="Esqueci minha senha" color="neutral" onPress={() => handleToPage('Recuperação de senha')}/>
           </S.Container>
         </S.Container>
       </S.Container>
