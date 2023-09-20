9f3680350e8750356362f097e0083f34106ab81c
BrunoSaade <brsaade@gmail.com>
2023-09-20 18:13:21 -0300

    Feat: Created card's component

---
+++ src/components/Cards/CardList/styles.ts
@@ -0,0 +1,9 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  card_list: {
+    flexDirection: 'row',
+    gap: 40,
+    paddingHorizontal: 30,
+  },
+})
