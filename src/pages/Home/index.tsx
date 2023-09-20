6e744d720bd6ba81e5d88f7d137c4eed73473dd7
BrunoSaade <brsaade@gmail.com>
2023-09-20 18:14:40 -0300

    Feat: Added more components to page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,28 @@
 import React from "react";
 import { S } from "../../styles/styles";
-import { Text } from "react-native";
+import RContainer from "../../components/RContainer";
+import RInput from "../../components/RInput";
+import Card from "../../components/Cards/Card";
+import CardList from "../../components/Cards/CardList";
+import { Image } from "react-native";
+import RButton from "../../components/RButton";
 
 export default function Home() {
   return (
-    <S.Container>
-      <Text>Ola</Text>
-    </S.Container>
+    <RContainer>
+      <S.Container>
+        <S.Container>
+          <S.Container customPaddingHorizontal="30px" customPaddingVertical="30px">
+            <RInput style={{height: 36}} fontSize={20} placeholder="Insira o termo da busca..."/>
+          </S.Container>
+          <S.Container style={{marginTop: 20}}>
+            <CardList />
+          </S.Container>
+          <S.Container customPaddingHorizontal="30px" customPaddingVertical="30px">
+            <RButton style={{marginTop: 30}} label="NOVA PESQUISA" color="success" onPress={() => ''}/>
+          </S.Container>
+        </S.Container>
+      </S.Container>
+    </RContainer>
   );
 }
