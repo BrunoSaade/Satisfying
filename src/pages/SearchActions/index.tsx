77dc7423fe806d289f382f73e24a0ef7c68c3066
BrunoSaade <brsaade@gmail.com>
2023-09-25 16:17:51 -0300

    Chore: Changed file name

--- src/pages/SearchActions/index.tsx
+++ src/pages/SearchActions/index.tsx
@@ -1,11 +1,6 @@
 import React from "react";
 import { S } from "../../styles/styles";
 import RContainer from "../../components/RContainer";
-import RInput from "../../components/RInput";
-import Card from "../../components/Cards/Card";
-import CardList from "../../components/Cards/CardList";
-import { Image } from "react-native";
-import RButton from "../../components/RButton";
 import Sections from "../../components/Sections";
 
 export default function SearchActions(props: any) {
