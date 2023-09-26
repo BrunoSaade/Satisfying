047456d9e803f5e48ca5e5d88c98da783fdaae6d
BrunoSaade <brsaade@gmail.com>
2023-09-26 11:37:42 -0300

    Feat: Added email validator

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { S } from "../../styles/styles";
 import { styles } from "./styles";
@@ -10,6 +10,19 @@ import RButton from "../../components/RButton";
 import RContainer from '../../components/RContainer';
 
 export default function Login(props: any) {
+
+  const [password, setPassword] = React.useState('');
+  const [email, setEmail] = React.useState('');
+  const [emailIsValid, setEmailIsValid] = React.useState(false);
+
+  useEffect(() => {
+    let reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w\w+)+$/;
+    if (reg.test(email) === false) {
+      setEmailIsValid(false)
+    } else {
+      setEmailIsValid(true)
+    }
+  }, [email]);
 
   function handleToPage(page: string) {
     props.navigation.push(page)
@@ -24,13 +37,24 @@ export default function Login(props: any) {
             <Icon name="sentiment-satisfied" size={60} color="white" />
           </S.Container>
           <S.Container style={{gap: 15}}>
-            <RInput label="E-mail" placeholder="E-mail"/>
-            <RInput label="Senha" placeholder="Digite sua senha" isPassword/>
+            <RInput label="E-mail" placeholder="E-mail" value={email} onChangeText={setEmail}/>
+            <RInput 
+              label="Senha" 
+              placeholder="Digite sua senha" 
+              value={password} 
+              onChangeText={setPassword} 
+              isPassword 
+              error={emailIsValid ? "" : "E-mail e/ou senha inválidos."}/>
             <RButton style={{marginTop: 30}} label="Entrar" color="success" onPress={() => handleToPage('Drawer')}/>
           </S.Container>
           <S.Container style={{marginTop: 70}}>
             <RButton customHeight={37} label="Criar minha conta" color="info" onPress={() => handleToPage('Nova Conta')}/>
-            <RButton style={{marginTop: 15}} customHeight={37} label="Esqueci minha senha" color="neutral" onPress={() => handleToPage('Recuperação de senha')}/>
+            <RButton 
+            style={{marginTop: 15}} 
+            customHeight={37} 
+            label="Esqueci minha senha" 
+            color="neutral" 
+            onPress={() => handleToPage('Recuperação de senha')}/>
           </S.Container>
         </S.Container>
       </S.Container>
