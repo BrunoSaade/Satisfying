38e55a28ca4b8afff49452922246fb773876ee5c
joaozin046 <joaoxpp2001@hotmail.com>
2023-09-28 18:48:47 -0300

    pagina de modificar + modal
    pagina de modificar + o modal para cancelar

--- src/pages/ModifySearch/index.tsx
+++ src/pages/ModifySearch/index.tsx
@@ -1,20 +1,60 @@
-import React from "react";
+import React,{useState} from 'react';
+
 import { S } from "../../styles/styles";
-import { Text } from "react-native";
-import RContainer from "../../components/RContainer";
 
-export default function ModifySearch(props: any) {
+import { theme } from '../../theme/styles';
+import RInput from "../../components/RInput";
+import RButton from "../../components/RButton";
+import RContainer from '../../components/RContainer';
+import RImagePicker from '../../components/RImagePicker';
+import { SvgXml } from 'react-native-svg';
+import { TouchableOpacity } from "react-native-gesture-handler";
+import {styles,svgApagarDados} from "./styles";
+import { View,Modal, Image, Text, StyleSheet } from 'react-native';
 
-  function handleToPage(page: string) {
-    props.navigation.push(page)
+export default function NewSearch(props: any) {
+
+  const [visible,setVisible]=useState(false);
+
+  const handleDeleteButtonPress = () => {
+    setVisible(true);
   }
 
   return (
     <RContainer>
       <S.Container>
-
-
+        <S.Container customWidth="653px" customPaddingVertical="30px">
+          <S.Container style={{gap: 15}}>
+            <RInput label="Nome" placeholder="Digite o nome da pesquisa"/>
+            <RInput label="Data" placeholder="Digite a data"/>
+            <RImagePicker />
+              <View style={styles.row}>
+                <RButton style={styles.column_save} label="SALVAR" color="success" onPress={() => ''}/>
+                <TouchableOpacity onPress={() => handleDeleteButtonPress()} style={styles.column_delete}>
+                  <SvgXml xml={svgApagarDados} />
+                  <S.TextDefault style={styles.text}>Apagar</S.TextDefault>
+                </TouchableOpacity>
+              </View >
+              <Modal animationType='fade' transparent={true} visible={visible}>
+                <View style={{flex: 1,flexDirection: 'column',justifyContent: 'center',alignItems: 'center'}}>
+                  <View style={{width: 506,height: 253 }}>
+                    <RContainer >
+                      <S.Container >
+                        <S.Container>
+                            <S.TextDefault style={styles.text_modal} > Tem certeza que deseja apagar essa pesquisa? </S.TextDefault>
+                            <View style= {styles.row}>
+                              <RButton label='SIM' color='error' onPress={() => setVisible(false)} style={styles.column_modal_confirm}/>
+                              <RButton label='CANCELAR' color="blue" onPress={() => setVisible(false)} style={styles.column_modal_cancel} />
+                          </View>
+                        </S.Container>
+                      </S.Container>
+                    </RContainer>
+                  </View>
+                </View>
+              </Modal>
+          </S.Container>
+        </S.Container>
       </S.Container>
-    </RContainer >
+    </RContainer>
   );
-}
+};
--- src/pages/ModifySearch/styles.ts
+++ src/pages/ModifySearch/styles.ts
@@ -2,7 +2,60 @@ import { StyleSheet } from 'react-native';
 import { theme } from '../../theme/styles';
 
 export const styles = StyleSheet.create({
+    view_logo: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: 10,
+      },
+      view_logo__text: {
+        fontSize: 40,
+        color: 'white',
+      },
+      row:{
+        flexDirection: 'row',
+        marginTop: 30
+      },
+      text: {
+        textAlign: 'center',
+        color: 'white',
+        fontSize: 23
+      },
+      column_save: {  
+        width:478,
+        marginRight: 103,
+        flexDirection: 'row',
+      },
+      column_delete: { 
+        alignItems: 'center'
+       },
+       modal:{
+       },
 
-
-
+      column_modal_confirm: { 
+        width: 216,
+        height:71,
+        marginLeft: 24,
+        marginTop: 30
+      },
+      column_modal_cancel: { 
+        width: 216,
+        height:71,
+        marginLeft: 20,
+        marginTop: 30
+      },
+      text_modal: {
+        textAlign: 'center',
+        color: 'white',
+        fontSize: 30,
+        width:403,
+        height:73,
+        marginLeft: 44,
+        marginTop: 29
+      }
 })
+
+export const svgApagarDados = `<svg width="28" height="30" viewBox="0 0 28 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M4.87496 30C4.18746 30 3.59892 29.7552 3.10933 29.2656C2.61975 28.776 2.37496 28.1875 2.37496 27.5V3.75H0.666626V1.25H8.49996V0H19.5V1.25H27.3333V3.75H25.625V27.5C25.625 28.1667 25.375 28.75 24.875 29.25C24.375 29.75 23.7916 30 23.125 30H4.87496ZM23.125 3.75H4.87496V27.5H23.125V3.75ZM9.29163 23.9167H11.7916V7.29167H9.29163V23.9167ZM16.2083 23.9167H18.7083V7.29167H16.2083V23.9167Z" fill="white"/>
+</svg>`
